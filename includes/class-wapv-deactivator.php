ceb61e07e2363c490595cb25256597428f844af8
Ahmad Wael <dev.ahmedwael@gmail.com>
2020-05-12 11:43:24 +0200

    initial commit

---
+++ includes/class-wapv-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/DevWael
+ * @since      1.0.0
+ *
+ * @package    Wapv
+ * @subpackage Wapv/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wapv
+ * @subpackage Wapv/includes
+ * @author     AHMAD WAEL <dev.ahmedwael@gmail.com>
+ */
+class Wapv_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
